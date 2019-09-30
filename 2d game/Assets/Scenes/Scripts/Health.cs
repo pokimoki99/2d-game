2f7f67dc02f7dcf9455b3a3d7c2b3ff505b635aa
pokimoki99 <48154115+pokimoki99@users.noreply.github.com>
2019-09-30 09:53:39 +0100

    fixed for 1.8

---
+++ 2d game/Assets/Scenes/Scripts/Health.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+
+    public int health;
+    public int numOfHearts;
+
+    public Image[] hearts;
+    public Sprite fullHearts;
+    public Sprite emptyHearts;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(health > numOfHearts)
+        {
+            health = numOfHearts;
+        }
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < health)
+            {
+                hearts[i].sprite = fullHearts;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHearts;
+            }
+            if(i < numOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false; 
+            }
+        }
+
+    }
+
+} 
+
+   
+
---
+++ 2d game/Assets/Scenes/Scripts/Score.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public static int scoreValue = 0;
+    public Text scoreText;
+    private float startTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreValue = (int)(Time.time - startTime);
+
+        scoreText.text = "Score : " + scoreValue;
+    }
+}
