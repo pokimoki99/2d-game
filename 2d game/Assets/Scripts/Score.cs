47e40580ed9cc6b06dec77b5bf018ec3bd9951dc
pokimoki99 <48154115+pokimoki99@users.noreply.github.com>
2019-09-30 09:40:34 +0100

    Revert "Score fix"
    This reverts commit 5750b3ecd92fc793df327dd52b009d9ae44eb535.

--- 2d game/Assets/Scripts/Score.cs
+++ 2d game/Assets/Scripts/Score.cs
@@ -6,18 +6,16 @@ public class Score : MonoBehaviour
 {
     public static int scoreValue = 0;
     public Text scoreText;
-    private float startTime;
     // Start is called before the first frame update
     void Start()
     {
         scoreText = GetComponent<Text>();
-        startTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        scoreValue = (int)(Time.time - startTime);
+        scoreValue++;
 
         scoreText.text = "Score : " + scoreValue;
     }
