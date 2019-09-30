0d3e2cde5d10b2335d1fd6a524f330b2d6b6c49d
pokimoki99 <48154115+pokimoki99@users.noreply.github.com>
2019-09-30 12:48:57 +0100

    working respawn
    remove hearts and move char

---
+++ 2d game/Assets/Scenes/prototype character/respawn_mechanic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respawn_mechanic : MonoBehaviour
+{
+    public GameObject detecc;//but also attack
+    public GameObject player;
+    Vector3 spawn;
+
+    public Health playerHealthScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        detecc = GameObject.FindGameObjectWithTag("Respawn");
+        player = GameObject.FindGameObjectWithTag("player");
+        spawn = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //GetComponent("Health.cs");
+        if (player.transform.position.y < detecc.transform.position.y)
+        {
+            
+            player.transform.position = spawn;
+            playerHealthScript.health -= 1;
+        }
+    }
+}
