2dfeb65a05544cf80bc6fa5cfc21bf0e3c64773b
ShadowKnight20 <48155299+ShadowKnight20@users.noreply.github.com>
2019-10-05 22:09:31 +0100

    Enemies V1
    Enemies spawn and move.

---
+++ 2d game/Assets/Scenes/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float enemyspeed = 5f;
+   // public GameObject Enemy;
+    //public GameObject detecc;
+  //  Vector3 spawn;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * enemyspeed * Time.deltaTime);
+    
+    }
+}
---
+++ 2d game/Assets/Scenes/Scripts/EnemySpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemy;
+    float randY;
+    Vector2 whereToSpawn;
+    public float spawnRate = 2f;
+    float nextSpawn = 0.0f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            randY = Random.Range(-0.0f, 0.0f);
+            whereToSpawn = new Vector2(randY, transform.position.y);
+            Instantiate(enemy, whereToSpawn, Quaternion.identity);
+        }
+    }
+}
