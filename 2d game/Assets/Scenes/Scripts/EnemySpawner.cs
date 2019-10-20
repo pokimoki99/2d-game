216b1f76dad1aa6994fa2354b3fc977969a9e496
pokimoki99 <48154115+pokimoki99@users.noreply.github.com>
2019-10-20 11:00:42 +0100

    mines spawned randomly

--- 2d game/Assets/Scenes/Scripts/EnemySpawner.cs
+++ 2d game/Assets/Scenes/Scripts/EnemySpawner.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject enemy;
+    public GameObject mine;
     public GameObject enemies;
     float randY;
     Vector2 whereToSpawn;
     public float spawnRate = 2f;
     float nextSpawn = 0.0f;
+    int num;
+
 
     //public Enemy enemy;
     public GameObject player;
@@ -17,6 +20,7 @@ public class EnemySpawner : MonoBehaviour
     void Start()
     {
         enemy = GameObject.FindGameObjectWithTag("enemy");
+        mine = GameObject.FindGameObjectWithTag("mine");
     }
 
     // Update is called once per frame
@@ -26,13 +30,28 @@ public class EnemySpawner : MonoBehaviour
         {
             nextSpawn = Time.time + spawnRate;
             randY = Random.Range(-0.0f, 0.0f);
-            whereToSpawn = new Vector2(player.transform.position.x+5, transform.position.y);
-            enemies = Instantiate(enemy, whereToSpawn, Quaternion.identity);
+            Random_pos();
+            if (num == 0)
+            {
+                whereToSpawn = new Vector2(player.transform.position.x + 5, transform.position.y);
+                enemies = Instantiate(enemy, whereToSpawn, Quaternion.identity);
+            }
+            if (num==1)
+            {
+                whereToSpawn = new Vector2(player.transform.position.x + 5, transform.position.y);
+                enemies = Instantiate(mine, whereToSpawn, Quaternion.identity);
+            }
         }
 
         if (enemies.transform.position.y <= -100)
         {
             Destroy(this);
         }
+
+    }
+    void Random_pos()
+    {
+        num = (Random.Range(0, 2));
+        Debug.Log(num);
     }
 }
