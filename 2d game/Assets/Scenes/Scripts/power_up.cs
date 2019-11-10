ac2302d39da2bbf267e6f917d77c6543f8249b6b
pokimoki99 <48154115+pokimoki99@users.noreply.github.com>
2019-11-10 12:34:35 +0000

    powerup and sorting places

---
+++ 2d game/Assets/Scenes/Scripts/power_up.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class power_up : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject tank_power_up;
+    public GameObject tank;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name.Equals("player"))
+        {
+            GameObject.Find("tank").renderer.enabled = true;
+            GameObject.Find("player").renderer.enabled = false;
+            Debug.Log("collide with player");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+
+    }
+}
