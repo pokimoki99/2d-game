8ddf08ed46b7b23469a5645ac89bdd96b157f0a0
ShadowKnight20 <kperovs15@gmail.com>
2019-11-18 10:31:43 +0000

    shooting v1.0
    Shoots forwards delets itself later, no effect after atm.

---
+++ 2d game/Assets/Scenes/Prefabs/Bullet_Script.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Script : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        Invoke("DestroySelf", .5f);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb2d.velocity = new Vector2(20, 0);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("enemy"))
+        {
+            DestroySelf();
+        }
+
+    }
+    private void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+}
