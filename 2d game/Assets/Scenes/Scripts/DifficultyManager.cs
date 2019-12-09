7eb2e8a9969b967b2ed249fc85deb8324f8ca28b
ShadowKnight20 <48155299+ShadowKnight20@users.noreply.github.com>
2019-12-09 16:12:08 +0000

    easy and medium is being called by the button.....
    Basicaly now it needs to be called in terrain generaor to spawn those bigger terrain if difficulty is medium.

---
+++ 2d game/Assets/Scenes/Scripts/DifficultyManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifficultyManager : MonoBehaviour
+{
+    public bool easy, medium, hard;
+    // Start is called before the first frame update
+    void Start()
+    {
+        easy = false;
+        medium = false;
+        hard = false;
+    }
+    public void DifficultySelect(int Difficulty)
+    {
+        if(Difficulty == 0)
+        {
+            easy = true;
+            medium = false;
+            Debug.Log("easy is true");
+        }
+
+        if(Difficulty == 1)
+        {
+            medium = true;
+            easy = false;
+            Debug.Log("medium is true");
+        }
+    }
+   
+
+
+    
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
